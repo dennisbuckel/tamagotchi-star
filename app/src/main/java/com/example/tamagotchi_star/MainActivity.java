4f9f8cd42589ab25c7dbdf78c983eded5080f4d6
dennisbuckel <dennis.buck@web.de>
2020-05-12 11:18:35 +0200

    commit test

--- app/src/main/java/com/example/tamagotchi_star/MainActivity.java
+++ app/src/main/java/com/example/tamagotchi_star/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.tamagotchi_star;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.*;
 
 public class MainActivity extends AppCompatActivity {
 
